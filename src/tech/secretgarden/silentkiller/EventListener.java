5b6590389c6b03b6f1423fba848614bfb47aa6e3
ChuckLingBao <95773999+ChuckLingBao@users.noreply.github.com>
2022-01-08 17:39:04 -0800

    Console deathmessage
    Added deathmessage to always output to console regardless of killer invisibility.

--- src/tech/secretgarden/silentkiller/EventListener.java
+++ src/tech/secretgarden/silentkiller/EventListener.java
@@ -1,5 +1,7 @@
 package tech.secretgarden.silentkiller;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +12,10 @@ public class EventListener implements Listener {
     public void death(PlayerDeathEvent e) {
         Player player = e.getEntity().getPlayer();
         if (player.getKiller().isInvisible()) {
+            CommandSender console = Bukkit.getConsoleSender();
+            String dm = e.getDeathMessage();
             e.setDeathMessage(null);
+            console.sendMessage(dm);
         }
     }
 }
